27641412fc9048ce7eee65cc3104f1f91a8f1d7e
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-01-06 19:04:03 -0300

    Este es el último commit de la página Lakeside. El html, css y js está terminado. La funcionalidad del arrowup en los amenities se agregó El resto de funcionalidades no se agregaran Esta página está terminada en versión desktop y movil, no en tablet o ipad Queda por definir las resoluciones de las imagenes en un programa de diseño como ph para mejorar el loading También queda por testear por futuros errores, asi como conseguir un dominio oficial.

--- app/carrousel.js
+++ app/carrousel.js
@@ -1,7 +1,6 @@
 "use strict";
 
-console.log("Hola Mundo");
-
+// Nav function
 const openMenu = document.getElementById("open_menu");
 const logo = document.querySelector(".nav_logo_link");
 const navMenu = document.querySelector(".nav_menu");
@@ -25,31 +24,14 @@ openMenu.addEventListener("click", function (e) {
     logo.classList.add("light_color");
   }
 });
-// Open menu function
-/*
-1. Logo a color negro
-2. Boton a color negro
-3. Girar boton 
-4. Desplazar de arriba a abajo el nav_menu
- 
-*/
-
-// Amenities all functions
-const amenitiesList = document.querySelector(".amenities_list");
-const arrowUp = document.getElementById("arrowUp");
-
-arrowUp.addEventListener("click", function (e) {
-  e.preventDefault;
-  amenitiesList.classList.toggle("hidden");
-  if (amenitiesList.classList.contains("hidden")) {
-    arrowUp.style.transform = "rotate(180deg)";
-  } else {
-    arrowUp.style.transform = "";
-  }
-});
-
-
-
+// Carrousel de imagenes
 const cabinImg = document.querySelector(".cabin--img");
 const carrousel = document.querySelector(".cabin_carousel");
 const carrouselItem = document.querySelectorAll(".cabin_carousel--img")
+
+/* 
+- Hacer click en el carrousel 
+- Imagen del carrousel pasa a la imagen principal 
+- La imagen principal pasa a el carrousel
+*/
+
